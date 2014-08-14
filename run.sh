9f0c72b4f782a8b203a076db44e5f07f432b5ca6
Ivan Inozemtsev <ivan.inozemtsev@xored.com>
2014-08-14 19:09:37 +0700

    initial contribution

---
+++ run.sh
@@ -0,0 +1,12 @@
+WORKSPACE=${WORKSPACE:-$PWD}
+echo "Start" > $WORKSPACE/out.txt
+
+for i in {1..60}
+do
+    sleep 1
+    echo "$i"    
+    echo "$i" >> $WORKSPACE/out.txt
+
+done
+
+echo "Finish" >> $WORKSPACE/out.txt
